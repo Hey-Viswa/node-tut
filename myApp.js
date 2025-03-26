f637e64e0ffa66157140e1a8f05692ce02ac7d1b
VISWARANJAN GIRI <goodlegiri892@gmail.com>
2025-03-26 16:30:46 +0000

    added the proper middle ware

--- myApp.js
+++ myApp.js
@@ -4,13 +4,11 @@ let app = express();
 // Serve static files from the public directory
 app.use('/public', express.static(__dirname + '/public'));
 
-
+// JSON API endpoint
 app.get("/json", function(req, res) {
     res.json({
         "message": "Hello json"
     });
 });
-
-// Add this line to start the server
 
 module.exports = app;
