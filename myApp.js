4e2dc27848fa5a1aa61e319fc5b49c3cf7e5254a
VISWARANJAN GIRI <goodlegiri892@gmail.com>
2025-03-26 16:28:47 +0000

    added /json

--- myApp.js
+++ myApp.js
@@ -4,13 +4,13 @@ let app = express();
 // Serve static files from the public directory
 app.use('/public', express.static(__dirname + '/public'));
 
-// global variable
-let absolutePath = __dirname + "/views/index.html";
 
 app.get("/json", function(req, res) {
-   res.json({
-   "message": "Hello json"
-   })
+    res.json({
+        "message": "Hello json"
+    });
 });
+
+// Add this line to start the server
 
 module.exports = app;
