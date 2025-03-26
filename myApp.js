899c293efa311e015fe4c67cf60cf213db0d22af
VISWARANJAN GIRI <goodlegiri892@gmail.com>
2025-03-26 16:04:44 +0000

    added

--- myApp.js
+++ myApp.js
@@ -2,11 +2,11 @@ let express = require('express');
 let app = express();
 
 // global variable
-absolutePath = _dirname + "/views/index.html";
+let absolutePath = __dirname + "/views/index.html";
 
 // Log message to console
 console.log("Hello World");
 
-app.get(absolutePath, (req, res) => {
-    res.sendfile(absolutePath);
+app.get("/", (req, res) => {
+    res.sendFile(absolutePath);
 });
