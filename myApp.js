a18ea65611b7a587c186c1acc50d955f38678373
VISWARANJAN GIRI <goodlegiri892@gmail.com>
2025-03-26 16:17:27 +0000

    added static file

--- myApp.js
+++ myApp.js
@@ -1,11 +1,14 @@
 let express = require('express');
 let app = express();
 
+// Serve static files from the public directory
+app.use('/public', express.static(__dirname + '/public'));
+
 // global variable
 let absolutePath = __dirname + "/views/index.html";
 
-
-
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/views/index.html");
-  });
+    res.sendFile(absolutePath);
+});
+
+module.exports = app;
